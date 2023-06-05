599ef373c7a9eb0301ae35138518b11ea758c662
brendan power <brendan.power@kellogg.northwestern.edu>
2023-06-05 03:53:01 +0000

    first commit after losing everything :(

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,13 +1,17 @@
 class PlacesController < ApplicationController
-
+  before_action :check_user, except: [:index]
   def index
     @places = Place.all
   end
 
+    
+ 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => @current_user["id"]})
   end
+  
+
 
   def new
     @place = Place.new
@@ -18,6 +22,13 @@ class PlacesController < ApplicationController
     @place["name"] = params["place"]["name"]
     @place.save
     redirect_to "/places"
+  end
+
+
+  private
+
+  def check_user
+    redirect_to "/" unless @current_user || request.path == "/"
   end
 
 end
